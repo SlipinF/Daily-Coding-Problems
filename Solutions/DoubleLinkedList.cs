1d7359190ba3b7a6b280c313fb8e1a9713dbf404
SlipinF <itzamnass@gmail.com>
2019-09-11 15:43:01 +0200

    DoubleLinkedListSolution

---
+++ Solutions/DoubleLinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfnumbers = new int[5] {1,4,3,4,1};
+
+            Console.WriteLine(ListCheck().ToString());
+            Console.ReadKey();
+
+            bool ListCheck()
+            {
+                bool result = false;
+                for (int i = 0; i < arrayOfnumbers.Length; i++)
+                {
+                    if (arrayOfnumbers[i] != arrayOfnumbers[arrayOfnumbers.Length -1 - i])
+                    {
+                        result = false;
+                        return result;
+                    }
+                    else
+                    {
+                        result = true;
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
